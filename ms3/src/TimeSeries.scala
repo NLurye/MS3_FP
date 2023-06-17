e23a098130aed863876761e6beafe70fef501258
NLurye <anastasia.lurye@gmail.com>
2023-06-17 21:01:21 +0300

    split not tested

--- ms3/src/TimeSeries.scala
+++ ms3/src/TimeSeries.scala
@@ -4,20 +4,25 @@ import scala.io.Source
 
 class TimeSeries(csvFileName:String) {
   val data=new mutable.HashMap[String,Vector[Double]]()
-  private val dataBuilder=new mutable.HashMap[String,mutable.ArrayBuffer[Double]]()
-  private val source=Source.fromFile(csvFileName)
-  //  val length = source.getLines().size
-  val features=source.getLines().next().split(",").toVector
+  var length: Int
+  var features: Vector[String]
+  private val source = if (csvFileName.nonEmpty) Some(Source.fromFile(csvFileName))
+    else None
+  if (source.isDefined) {
+    val dataBuilder=new mutable.HashMap[String,mutable.ArrayBuffer[Double]]()
+    features=source.get.getLines().next().split(",").toVector
   features.foreach(f=>dataBuilder.put(f,mutable.ArrayBuffer[Double]()))
-  source.getLines().foreach(line=>{
+  source.get.getLines().foreach(line=>{
         val row=line.split(",")
         for(i<-0 until row.length) {
           if(dataBuilder.contains(features(i)))
             dataBuilder.get(features(i)).get += row(i).toDouble
         }
   })
-  source.close()
-  dataBuilder.foreach(t=>data.put(t._1,t._2.toVector))
+    length = source.get.getLines().size
+  source.get.close()
+dataBuilder.foreach(t=>data.put(t._1,t._2.toVector))
+}
 
   // given name of a feature return in O(1) its value series
   def getValues(feature:String):Option[Vector[Double]]=
@@ -40,9 +45,30 @@ class TimeSeries(csvFileName:String) {
     else
       None
 
-  def this(data: Map[String, Vector[Double]]) {
+  def this(data: Map[String, Vector[Double]], old_length: Int, old_features: Vector[String]) {
     this("")
     this.data ++= data
+    this.length = old_length
+    this.features = old_features
   }
-  
+
+  def split(n: Int): List[TimeSeries] = {
+    val numRows = length
+    val chunkSize = numRows / n
+    var remainder = numRows % n
+
+    var startIndex = 0
+    val splitSeries = for (_ <- 1 to n) yield {
+      val endIndex = startIndex + chunkSize + (if (remainder > 0) 1 else 0)
+      val chunkRange = startIndex until endIndex
+      val chunkData = data.map { case (feature, values) =>
+        feature -> values.slice(chunkRange.start, chunkRange.end)
+      }
+      startIndex = endIndex
+      remainder -= 1
+      new TimeSeries(chunkData.toMap, chunkRange.length, features)
+    }
+    splitSeries.toList
+  }
+
 }
