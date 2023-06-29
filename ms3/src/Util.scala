9cbdf59593f54c9233a7dc956f352588ac2d60d4
anastasia <anastasia.lurye@lean-ai-tech.com>
2023-06-29 20:43:42 +0300

    grisha's fix

--- ms3/src/Util.scala
+++ ms3/src/Util.scala
@@ -2,29 +2,6 @@ import scala.math.log
 import scala.reflect.ClassTag
 
 object Util {
-
-  def max[A](as: List[A], comp: (A, A) => Int): A = {
-    var mx = as.head
-    if (as.size == 1)
-      return mx
-    val a = max(as.tail, comp)
-    if (comp(mx, a) < 0)
-      mx = a
-    mx
-  }
-
-  def map[A, B, C](as: List[A], fab: A => B, fbc: B => C): List[C] =
-    as.map(fab).map(fbc)
-
-  def isSorted[A](as: List[A], ordered: (A, A) => Boolean): Boolean = {
-    if (as.size == 1)
-      return true
-    if (ordered(as.head, as.tail.head))
-      isSorted(as.tail, ordered)
-    else
-      false
-  }
-
   def probs(xs: Array[Double]): Array[Double] =
     xs.map(xi => xs.count(xj => xi == xj) / xs.size.toDouble)
   def entropy(xs: Array[Double]): Double =
@@ -51,21 +28,10 @@ object Util {
   }
 
   def getMaximumAnomaly(xs: Array[Double]): (Double, Int) = {
-    var maxAnomaly = Double.MinValue
-    var maxIndex = 0
-    var i = 0
     val general_entropy = entropy(xs)
-//    println("general: ",general_entropy)
-    while (i < xs.length) {
-      val currentAnomaly = anomaly(xs, i, general_entropy)
-      if (currentAnomaly > maxAnomaly) {
-//        println("updated from: ", maxAnomaly, "to: ", currentAnomaly)
-        maxAnomaly = currentAnomaly
-        maxIndex = i
-      }
-      i += 1
-    }
-    (maxAnomaly, maxIndex)
+    val anomalies = (Range(0, xs.length)).map(pi => anomaly(xs, pi, general_entropy))
+//    println("max: ",anomalies.max, "i",anomalies.indexOf(anomalies.max))
+    (anomalies.max, anomalies.indexOf(anomalies.max))
   }
 
 }
